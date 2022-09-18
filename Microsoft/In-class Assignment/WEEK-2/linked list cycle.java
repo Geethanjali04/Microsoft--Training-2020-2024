7e3d3206eee4b52bc5919300ddfb660df79c92cb
Geethanjali <108317700+Geethanjali04@users.noreply.github.com>
2022-09-18 21:49:03 +0530

    Create linked list cycle.java

---
+++ Microsoft/In-class Assignment/WEEK-2/linked list cycle.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) { 
+        
+        ListNode slow =head;
+        if(head==null)
+        {
+            return false;
+        } 
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null)
+        {
+            if(slow==fast)
+            {
+                return true;
+            } 
+            slow=slow.next;
+            fast=fast.next.next;
+        } 
+        return false;
+    } 
+    
+}
