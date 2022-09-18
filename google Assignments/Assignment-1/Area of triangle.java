eb7693e749bf6e5a6f3fc458137e8f8cc10f5953
Geethanjali <108317700+Geethanjali04@users.noreply.github.com>
2022-09-18 20:06:18 +0530

    Update Area of triangle.java

--- google Assignments/Assignment-1/Area of triangle.java
+++ google Assignments/Assignment-1/Area of triangle.java
@@ -9,7 +9,7 @@ public class Main
 		int y2 = scanner.nextInt();
 		int x3 = scanner.nextInt();
 		int y3 = scanner.nextInt();
-		double area1 = Math.abs(x1*(y2-y3)+x2*(y1-y3)+x3*(y1-y2));
-		System.out.println(area1/2);
-		double area2 = Math.abs(x1*(y1-y2)+x2*(y1-y3)+x3*(y1-y2));
-		System.out.println(area2/2);
+		double area = Math.abs(x1*(y2-y3)+x2*(y1-y3)+x3*(y1-y2));
+		System.out.println(area);
+	}
+}
