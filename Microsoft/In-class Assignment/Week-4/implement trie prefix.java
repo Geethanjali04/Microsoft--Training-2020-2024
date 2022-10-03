4953a494fcaa102169a7167acd51b56832475196
Geethanjali <108317700+Geethanjali04@users.noreply.github.com>
2022-10-03 08:23:59 +0530

    Create implement trie prefix.java

---
+++ Microsoft/In-class Assignment/Week-4/implement trie prefix.java
@@ -0,0 +1,66 @@
+class Trie {
+    Node root;
+    class Node{
+        Node[]arr;
+        boolean end; 
+        public Node(){
+          arr=new Node[26];
+          end=false;
+        }
+    }
+    public Trie() {
+        root=new Node();
+    }
+    
+    public void insert(String word) { 
+        Node temp=root; 
+        for(int i=0;i<word.length();i++)
+        {
+            char curr=word.charAt(i); 
+            if(temp.arr[curr-'a']==null)
+            {
+                temp.arr[curr-'a']=new Node();
+            } 
+            temp=temp.arr[curr-'a'];
+        }
+        temp.end=true;
+    }
+    
+    public boolean search(String word) {
+        Node temp=root;
+        for(int i=0;i<word.length();i++)
+        { 
+            char curr=word.charAt(i);
+            if(temp.arr[curr-'a']==null)
+            {
+                return false;
+            }  
+            temp=temp.arr[curr-'a']; 
+        } 
+        return temp.end==true;
+    }  
+    
+    public boolean startsWith(String prefix) { 
+        Node temp=root;
+        for(int i=0;i<prefix.length();i++)
+        {
+            char curr=prefix.charAt(i);
+            if(temp.arr[curr-'a']==null)
+            {
+               return false; 
+            } 
+            temp=temp.arr[curr-'a'];
+            
+        } 
+        return true;
+        
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
