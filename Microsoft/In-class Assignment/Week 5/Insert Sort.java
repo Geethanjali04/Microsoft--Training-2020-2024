f54674a795f0c12bf56db1ae023ebe15f09337d0
Geethanjali <108317700+Geethanjali04@users.noreply.github.com>
2022-10-12 11:04:20 +0530

    Create Insert Sort.java

---
+++ Microsoft/In-class Assignment/Week 5/Insert Sort.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+         
+      //  insert sort
+      if(nums.length==0)
+      { 
+          return nums;
+      } 
+      else 
+       {
+            insertionsort(nums);
+            return nums;
+       }  
+    } 
+    public void insertionsort(int nums[])
+    {
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                if(nums[j-1]>nums[j])
+                {
+                    swap(j,j-1,nums);
+                }
+            }
+        } 
+    }
+       public void swap(int j,int i,int nums[])
+       {
+           int temp=nums[j]; 
+           nums[j]=nums[i];
+           nums[i]=temp;
+           
+       }
+       
+    
+}
